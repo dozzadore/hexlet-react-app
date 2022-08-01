6dd906892a879982899b79dbc969ca48a1277a23
dozzadore <dozzadore@gmail.com>
2022-08-01 22:00:59 +0300

    create components(Main, Navbar) add css styles(main.css, navbar.css)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Navbar from './components/Navbar/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Navbar />
+        <Main />
       </div>
     );
   }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './navbar.css';
+
+class Navbar extends Component{
+    render() {
+    return (
+        <div className="Navbar">Navbar</div>
+    );
+ }
+}
+
+
+export default Navbar;
